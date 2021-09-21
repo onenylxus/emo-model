f8ce8b422cb835e93266f1ff81762f4da33e5f74
Nicholas Ng <onenylxus@gmail.com>
2021-09-21 14:46:22 +0800

    Plot save

--- interface.py
+++ interface.py
@@ -1,18 +1,37 @@
 # Import
+import numpy as np
+import os
 import matplotlib.pyplot as plt
+
 
 # Interface class
 class Interface:
   # Constructor
-  def __init__(self, opt):
-    self.opt = opt      # Optimizer
+  def __init__(self, opt, name, folder):
+    self.opt = opt       # Optimizer
+    self.name = name     # Model name
+    self.folder = folder # Folder name
 
-    self.positions = [] # Position records
-    self.values = []    # Value records
-    self.trend = []     # Solution trend
+    self.positions = []  # Position records
+    self.values = []     # Value records
+    self.trend = []      # Solution trend
+
+    self.dirb = True     # Directory boolean
+
+    # Create missing directories
+    if self.dirb:
+      if not os.path.exists(f'{os.getcwd()}/contours'):
+        os.mkdir(f'{os.getcwd()}/contours')
+        print('Contour folder created')
+      if not os.path.exists(f'{os.getcwd()}/contours/{self.folder}'):
+        os.mkdir(f'{os.getcwd()}/contours/{self.folder}')
+        print(f'{self.folder} folder created in contour folder')
+      print('')
+    self.dirb = False
 
     # Print interface header
     print('Electromagnetism-like Optimization Model')
+    print(f'Model: {self.name}\n')
     self.record()
 
   # Record
@@ -25,12 +44,34 @@ class Interface:
     print(f'Iteration {l}:')
     for i in range(self.opt.size):
       print(f'  Particle {i + 1}: {self.positions[l][i]}, value = {self.values[l][i]}')
-    print(f'  Best particle is particle {self.opt.population.best().index + 1} with value {self.trend[l]}')
+    print(f'  Best particle is particle {self.opt.solution().index + 1} with value {self.trend[l]}\n')
 
   # Plot objective function value
   def plot_ofv(self):
     plt.figure()
-    plt.plot(self.trend, linewidth=2)
+    plt.plot(self.trend, linewidth=4)
     plt.grid()
     plt.xlabel('Iterations')
     plt.ylabel('Objective function minimum')
+    plt.show()
+    plt.savefig('contours/%s/ofv.png' %(self.folder), format='png')
+    plt.clf()
+
+  # Plot contour
+  def plot_contour(self, f):
+    if self.opt.dim == 2:
+      xr = np.linspace(self.opt.lower[0], self.opt.upper[0], 50)
+      yr = np.linspace(self.opt.lower[1], self.opt.upper[1], 50)
+      X, Y = np.meshgrid(xr, yr)
+      Z = f(X, Y)
+
+      plt.figure()
+      plt.contour(X, Y, Z, linewidths=2)
+      plt.scatter(self.positions[self.opt.iter][:, 0], self.positions[self.opt.iter][:, 1], marker='x')
+      plt.plot(self.opt.solution().pos[0], self.opt.solution().pos[1], 'ro')
+      plt.title(f'Iteration {self.opt.iter}')
+      plt.xlim(self.opt.lower[0], self.opt.upper[0])
+      plt.ylim(self.opt.lower[1], self.opt.upper[1])
+      plt.show()
+      plt.savefig('contours/%s/iter%03d.png' %(self.folder, self.opt.iter), format='png')
+      plt.clf()
--- rosenbrock.py
+++ rosenbrock.py
@@ -6,13 +6,17 @@ from optimizer import Optimizer
 def rosenbrock(pos):
   return (1 - pos[0]) ** 2 + 100 * ((pos[1] - pos[0] ** 2) ** 2)
 
+def rosenbrockV(x, y):
+  return (1 - x) ** 2 + 100 * (y - x ** 2) ** 2
 
 # Execution script
 if __name__ == '__main__':
   o = Optimizer(2, [-2, -2], [2, 2], 10, rosenbrock, None, None)
-  i = Interface(o)
+  i = Interface(o, 'Rosenbrock function', 'rosenbrock')
+  i.plot_contour(rosenbrockV)
 
-  while i.opt.iter < 100:
+  while i.opt.iter < 50:
     i.opt.iterate()
     i.record()
+    i.plot_contour(rosenbrockV)
   i.plot_ofv()
